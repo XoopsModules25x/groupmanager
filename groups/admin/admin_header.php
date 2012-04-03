68e17fca2573f257a7763b71b37dfb05ffbe4b58
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-04-03 07:24:58 +0000

    Updating to version 1.02 RC

--- groups/admin/admin_header.php
+++ groups/admin/admin_header.php
@@ -21,5 +21,5 @@ $pathModuleAdmin = $xoopsModule->getInfo('dirmoduleadmin');
 if ( file_exists($GLOBALS['xoops']->path($pathModuleAdmin.'/moduleadmin.php'))){
         include_once $GLOBALS['xoops']->path($pathModuleAdmin.'/moduleadmin.php');
     }else{
-        redirect_header("../../../admin.php", 5, _AM_GMAPS_MODULEADMIN_MISSING, false);
+        redirect_header("../../../admin.php", 5, _AM_GROUPS_MODULEADMIN_MISSING, false);
     }
