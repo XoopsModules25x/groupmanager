e0b0d1ccf69addc42af20e8958f5bbd836dfabbb
mambax7 <mambax7@gmail.com>
2021-06-06 16:31:03 -0400

    replace index.html with index.php

---
+++ class/index.php
@@ -0,0 +1,12 @@
+<?php
+
+header('HTTP/1.0 404 Not Found');
+
+<
+span style = 'color : #008000;' > Available . </span >
+<span style = 'color : #ff0000;' > Available . </span >
+
+<span style = "font-weight: bold;" ><span style = "color: #0000ff;" >
+
+
+<span style = 'color : #ff0000; font-weight : bold;' > value  </span >
